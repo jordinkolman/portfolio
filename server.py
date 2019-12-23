69db5f779652699d4fc4942772d6af67f2215c07
Jordin Kolman <jordinkolman@gmail.com>
2019-12-23 16:02:46 -0600

    added proper copy and made contact page email me

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
+import smtplib
+from email.message import EmailMessage
+from pathlib import Path
+
 app = Flask(__name__)
 
 
@@ -13,6 +17,7 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+'''
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
         email = data["email"]
@@ -29,6 +34,21 @@ def write_to_csv(data):
         csv_writer = csv.writer(database2, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
+'''
+
+
+def email_to_recipient(data):
+    email = EmailMessage()
+    email['to'] = 'jordinkolman@gmail.com'
+    email['from'] = data['email']
+    email['subject'] = data['subject']
+    email.set_content(data['message'])
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('jordinkolmanportfoliosite@gmail.com', 'devsite123')
+        smtp.send_message(email)
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
@@ -36,9 +56,9 @@ def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
-            write_to_csv(data)
+            email_to_recipient(data)
             return redirect('/thankyou.html')
         except:
-            return 'did not save to database'
+            return 'Email did not send. Please try again.'
     else:
         return 'Something went wrong. Please try again.'
